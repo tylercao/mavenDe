293fab6fa511c4d33b8655776b1b4f7105afa78d
tyler <tylercaolinhua@gmail.com>
2014-02-19 21:40:02 +0800

    test123

--- src/main/java/action/BaseController.java
+++ src/main/java/action/BaseController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
  
 @Controller
-@RequestMapping("/")
 public class BaseController {
  
 	@RequestMapping(value="/welcome", method = RequestMethod.GET)
